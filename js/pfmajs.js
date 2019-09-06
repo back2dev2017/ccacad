f3de737123123f30eb514d8c6591d26fa4962f7d
Charlie Coleman <colemanc@acm.org>
2019-09-06 06:28:12 -0400

    home 9/5/2019

--- js/pfmajs.js
+++ js/pfmajs.js
@@ -54,11 +54,11 @@ function selecttopsection (btnclicked) {
 			$(".academy-mgmt-sect").removeClass("hidediv");
 			break;
 		case 'acaddash':
-		$(".dashboard-sect").removeClass("hidediv");
-		gen_attendance_chart();
-		gen_progress_chart();
-		gen_via_chart();
-		gen_1on1_chart();
+      $(".dashboard-sect").removeClass("hidediv");
+      gen_attendance_chart();
+      gen_progress_chart();
+      gen_via_chart();
+      gen_1on1_chart();
 			break;
 		case 'acadrpts':
 			$(".reports-sect").removeClass("hidediv");
@@ -85,16 +85,19 @@ function allnavclick (tabclicked) {
 	
 	switch (btnval) {
 		case 'curriculums':
+      console.log('should have unhidden curriculums-disp');
 			$(".curriculums-disp").removeClass("hidediv");
 			break;
 		case 'courses':
-			$(".course-container").removeClass("hidediv");
-			console.log('should have shown the course');
-			if ( ! $.fn.DataTable.isDataTable ('#course-list-tbl')) {
-				build_course_tbl(window.dataobj.courses);
-			} else {
-				$('#course-list-tbl').DataTable().columns().adjust().draw();
-			}
+			$(".courses-disp").removeClass("hidediv");
+			if ( $.fn.DataTable.isDataTable ('#course-detail-list-tbl')) {
+        destroy_datatable('#course-detail-list-tbl');
+        destroy_datatable('#detail-roster-list-tbl');
+      }
+      build_sel_course_tbl(window.dataobj.course_selected);
+      build_sel_course_roster_tbl(window.dataobj.course_roster);
+      // $('.course-grid').width('300px');
+      // $('#course-list-tbl tbody td').width('300px');
 			break;
 		case 'surveys':
 			$(".surveys-disp").removeClass("hidediv");
@@ -116,17 +119,17 @@ function build_user_tbl (rsltdata) {
 		// var tblhtpx = tblht.toString() + "px";
 		// $("#user-list-tbl").removeAttr('width').DataTable( {
 	$("#user-list-tbl").DataTable( {
-			"bInfo": false,
-			"bFilter": false,
-			autoWidth: true,
-			responsive: true,
+		"bInfo": false,
+		"bFilter": false,
+		autoWidth: true,
+		responsive: true,
 		scrollY: "300px",
 		scrollX: true,
 		scrollCollapse: true,
 		stateSave: true,
-			paging: false,
-			"data": rsltdata,
-		rowId: 'id',
+		paging: false,
+		"data": rsltdata,
+    rowId: 'id',
 		// note the passing of 'id' as the second column - the reason is for the render, and the resulting link set up
 			columns: [
 				{ data: "id", "width": "20px", "title": "ID", className: "q-cent" }, 
@@ -229,7 +232,7 @@ function build_course_tbl (rsltdata) {
 		"bFilter": false,
 		autoWidth: true,
 		responsive: true,
-		scrollY: "300px",
+		scrollY: "260px",
 		scrollX: true,
 		scrollCollapse: true,
 		stateSave: true,
@@ -776,6 +779,150 @@ function make_item_id_link (data, type, row, meta) {
 	return linkret;
 }
 
+function gen_course_content(course_id) {
+	// filter out the content of just the desired course
+	let newdata = window.dataobj.course_content.filter(function (e) { return e.acad_id == course_id });
+
+	let tblarray = [];
+	let tmpobj = {};
+	// go through 52 weeks, looking for each week_num equal to the current week. Do it in batches of 13,
+	// each 13 is an 'object' in the object array to be used by JQ DataTable. Have to do it this way because
+	// it is possible some weeks will have nothing - so look for each week
+	for (nrow=1; nrow <= 4; nrow++) {
+		for (ncol=1; ncol <= 13; ncol++) {
+			let chtmlstuff = '<div class=gridcell>';
+			let calcwk = ncol + ((nrow - 1) * 13);
+			let colname = 'subweek' + ncol.toString();
+			let tmpa = newdata.filter(function (item) { return item.week_num == calcwk });
+			for (ix=0; ix < tmpa.length; ix++) {
+				// put text together for display
+				chtmlstuff = chtmlstuff + "<p class='gridtext'><strong>" + tmpa[ix].sess_title + "</strong> - " + tmpa[ix].module_name + "</p>";
+			}
+			// now that all items of a single week have been merged, add the 'column' to the DataTable array object
+			chtmlstuff = chtmlstuff + "</div>";
+			tmpobj[colname] = chtmlstuff;
+		}
+		// now that all the 13 weeks of the quarter have been merged, "push" the "row" into the DataTable array
+		tmpobj['quarter'] = nrow;
+		tblarray.push(tmpobj);
+		tmpobj = {};
+	}
+	window.dataobj.course_selected = tblarray;
+	// build_sel_course_tbl(window.dataobj.course_selected);
+	// build_sel_course_roster_tbl(window.dataobj.course_roster);
+}
+
+function build_sel_course_tbl (rsltdata) {
+	// var tblht = $("#user-list-tbl").height() - 88;
+	// var tblhtpx = tblht.toString() + "px";
+	// $("#user-list-tbl").removeAttr('width').DataTable( {
+	$("#course-detail-list-tbl").removeAttr('.width').DataTable( {
+	"bInfo": false,
+	"bFilter": false,
+	autoWidth: true,
+	responsive: true,
+	scrollY: "350px",
+	scrollX: true,
+	scrollCollapse: true,
+	stateSave: true,
+	paging: false,
+	"data": rsltdata,
+	// note the passing of 'id' as the second column - the reason is for the render, and the resulting link set up
+	columns: [
+		{ data: "quarter", "width": "50px", "title": "Quarter", className: "q-cent" },
+		{ data: "subweek1", "width": "200px", "title": "Week 1", className: "course-grid" },
+		{ data: "subweek2", "width": "200px", "title": "Week 2", className: "course-grid" },
+		{ data: "subweek3", "width": "200px", "title": "Week 3", className: "course-grid" },
+		{ data: "subweek4", "width": "200px", "title": "Week 4", className: "course-grid" },
+		{ data: "subweek5", "width": "200px", "title": "Week 5", className: "course-grid" },
+		{ data: "subweek6", "width": "200px", "title": "Week 6", className: "course-grid" },
+		{ data: "subweek7", "width": "200px", "title": "Week 7", className: "course-grid" },
+		{ data: "subweek8", "width": "200px", "title": "Week 8", className: "course-grid" },
+		{ data: "subweek9", "width": "200px", "title": "Week 9", className: "course-grid" },
+		{ data: "subweek10", "width": "200px", "title": "Week 10", className: "course-grid" },
+		{ data: "subweek11", "width": "200px", "title": "Week 11", className: "course-grid" },
+		{ data: "subweek12", "width": "200px", "title": "Week 12", className: "course-grid" },
+		{ data: "subweek13", "width": "200px", "title": "Week 13", className: "course-grid" }
+	] }
+	);
+	$('#course-detail-list-tbl td').click(function() {
+		var column_num = parseInt( $(this).index() ) + 1;
+		var row_num = parseInt( $(this).parent().index() ) + 1;
+		let weekpick = ((row_num - 1) * 13) + (column_num - 1);
+		console.log(row_num, column_num, weekpick);
+	});
+	$('#course-detail-list-tbl td').dblclick(function() {
+		var column_num = parseInt( $(this).index() ) + 1;
+		var row_num = parseInt( $(this).parent().index() ) + 1;
+		let weekpick = ((row_num - 1) * 13) + (column_num - 1);
+		console.log('user double-clicked', row_num, column_num, weekpick);
+	});
+}
+
+function build_sel_course_roster_tbl (rsltdata) {
+	// var tblht = $("#user-list-tbl").height() - 88;
+	// var tblhtpx = tblht.toString() + "px";
+	// $("#user-list-tbl").removeAttr('width').DataTable( {
+	$("#detail-roster-list-tbl").DataTable( {
+		"bInfo": false,
+		"bFilter": false,
+		autoWidth: true,
+		responsive: true,
+		scrollY: "240px",
+		scrollX: true,
+		scrollCollapse: true,
+		stateSave: true,
+		paging: false,
+		"data": rsltdata,
+		// note the passing of 'id' as the second column - the reason is for the render, and the resulting link set up
+		columns: [
+			{ data: "fname", "width": "100px", "title": "First Name" },
+			{ data: "lname", "width": "100px", "title": "Last Name" },
+			{ data: "att_id_use", "width": "77px", "title": "ID" },
+			{ data: "enroll_date", "width": "100px", "title": "Enrolled"},
+			{ data: "drop_date", "width": "100px", "title": "Drop Date"},
+			{ data: "id", "width": "40px", "title": "ID", "visible": false}
+		] }
+	);
+	$('#detail-roster-list-tbl td').click(function() {
+		let tblref = $('#detail-roster-list-tbl').DataTable();
+		let rowidx = tblref.cell( this ).index().row;
+		// recall that a 'row' in a jquery DataTable is an object - a rather complex object, so get a reference to it here
+		let refinfo = tblref.rows(rowidx).data();
+		// may want to console.log() the refinfo item if there is a problem with understanding what is available
+		// $("#name1o1").html(refinfo[0].fname + ' ' + refinfo[0].lname);
+		show_roster_1o1(refinfo);
+		$('#detail-roster-list-tbl tbody tr').removeClass('row-selected');
+		$(this).parent().addClass('row-selected');
+		// add row selection highlight stuff - remove existing highlight, then add back
+		var column_num = parseInt( $(this).index() ) + 1;
+		var row_num = parseInt( $(this).parent().index() ) + 1;
+	});
+}
+
+
+function show_roster_1o1(refinfo) {
+	// get the data for the person from the global object array
+	// $('#name1o1').html(
+	// 		'<p>' + refinfo[0].fname + ' ' + refinfo[0].lname + '</p>' +
+	// 		'<p>ID: ' + refinfo[0].id + '</p>'
+	// 		);
+	$('#name1o1').remove();
+	if ((refinfo[0].id % 2) == 1) {
+		$(".samp1o1").removeClass('hidediv');
+	} else {
+		$(".samp1o1").addClass('hidediv');
+	}
+
+	$('#via1').remove();
+	$('.viasamp').removeClass('hidediv');
+	if ((refinfo[0].id % 3) == 1) {
+		$('.viasamp').html('1 VIA');
+	} else {
+		$('.viasamp').html('No VIA yet');
+	}
+
+}
 
 function ed_data(p_itemid, p_dtref, editmode = "E") {
 	// p_itemid = item being edited, p_dtref = reference to DataTable, editmode = E for editing / A for adding
@@ -952,151 +1099,6 @@ function destroy_datatable(dtdivref) {
 		$(dtdivref).DataTable().destroy();
 		$(dtdivref).html('');
 	}	
-}
-
-function gen_course_content(course_id) {
-	// filter out the content of just the desired course
-	let newdata = window.dataobj.course_content.filter(function (e) { return e.acad_id == course_id });
-
-	let tblarray = [];
-	let tmpobj = {};
-	// go through 52 weeks, looking for each week_num equal to the current week. Do it in batches of 13,
-	// each 13 is an 'object' in the object array to be used by JQ DataTable. Have to do it this way because
-	// it is possible some weeks will have nothing - so look for each week
-	for (nrow=1; nrow <= 4; nrow++) {
-		for (ncol=1; ncol <= 13; ncol++) {
-			let chtmlstuff = '<div class=gridcell>';
-			let calcwk = ncol + ((nrow - 1) * 13);
-			let colname = 'subweek' + ncol.toString();
-			let tmpa = newdata.filter(function (item) { return item.week_num == calcwk });
-			for (ix=0; ix < tmpa.length; ix++) {
-				// put text together for display
-				chtmlstuff = chtmlstuff + "<p class='gridtext'><strong>" + tmpa[ix].sess_title + "</strong> - " + tmpa[ix].module_name + "</p>";
-			}
-			// now that all items of a single week have been merged, add the 'column' to the DataTable array object
-			chtmlstuff = chtmlstuff + "</div>";
-			tmpobj[colname] = chtmlstuff;
-		}
-		// now that all the 13 weeks of the quarter have been merged, "push" the "row" into the DataTable array
-		tmpobj['quarter'] = nrow;
-		tblarray.push(tmpobj);
-		tmpobj = {};
-	}
-	window.dataobj.course_selected = tblarray;
-	build_sel_course_tbl(window.dataobj.course_selected);
-	build_sel_course_roster_tbl(window.dataobj.course_roster);
-}
-
-function build_sel_course_tbl (rsltdata) {
-	// var tblht = $("#user-list-tbl").height() - 88;
-	// var tblhtpx = tblht.toString() + "px";
-	// $("#user-list-tbl").removeAttr('width').DataTable( {
-	$("#course-detail-list-tbl").DataTable( {
-	"bInfo": false,
-	"bFilter": false,
-	autoWidth: true,
-	responsive: true,
-	scrollY: "400px",
-	scrollX: true,
-	scrollCollapse: true,
-	stateSave: true,
-	paging: false,
-	"data": rsltdata,
-	// note the passing of 'id' as the second column - the reason is for the render, and the resulting link set up
-	columns: [
-		{ data: "quarter", "width": "50px", "title": "Quarter", className: "q-cent" },
-		{ data: "subweek1", "width": "200px", "title": "Week 1", className: "course-grid" },
-		{ data: "subweek2", "width": "200px", "title": "Week 2", className: "course-grid" },
-		{ data: "subweek3", "width": "200px", "title": "Week 3", className: "course-grid" },
-		{ data: "subweek4", "width": "200px", "title": "Week 4", className: "course-grid" },
-		{ data: "subweek5", "width": "200px", "title": "Week 5", className: "course-grid" },
-		{ data: "subweek6", "width": "200px", "title": "Week 6", className: "course-grid" },
-		{ data: "subweek7", "width": "200px", "title": "Week 7", className: "course-grid" },
-		{ data: "subweek8", "width": "200px", "title": "Week 8", className: "course-grid" },
-		{ data: "subweek9", "width": "200px", "title": "Week 9", className: "course-grid" },
-		{ data: "subweek10", "width": "200px", "title": "Week 10", className: "course-grid" },
-		{ data: "subweek11", "width": "200px", "title": "Week 11", className: "course-grid" },
-		{ data: "subweek12", "width": "200px", "title": "Week 12", className: "course-grid" },
-		{ data: "subweek13", "width": "200px", "title": "Week 13", className: "course-grid" }
-	] }
-	);
-	$('#course-detail-list-tbl td').click(function() {
-		var column_num = parseInt( $(this).index() ) + 1;
-		var row_num = parseInt( $(this).parent().index() ) + 1;
-		let weekpick = ((row_num - 1) * 13) + (column_num - 1);
-		console.log(row_num, column_num, weekpick);
-	});
-	$('#course-detail-list-tbl td').dblclick(function() {
-		var column_num = parseInt( $(this).index() ) + 1;
-		var row_num = parseInt( $(this).parent().index() ) + 1;
-		let weekpick = ((row_num - 1) * 13) + (column_num - 1);
-		console.log('user double-clicked', row_num, column_num, weekpick);
-	});
-}
-
-function build_sel_course_roster_tbl (rsltdata) {
-	// var tblht = $("#user-list-tbl").height() - 88;
-	// var tblhtpx = tblht.toString() + "px";
-	// $("#user-list-tbl").removeAttr('width').DataTable( {
-	$("#detail-roster-list-tbl").DataTable( {
-		"bInfo": false,
-		"bFilter": false,
-		autoWidth: true,
-		responsive: true,
-		scrollY: "240px",
-		scrollX: true,
-		scrollCollapse: true,
-		stateSave: true,
-		paging: false,
-		"data": rsltdata,
-		// note the passing of 'id' as the second column - the reason is for the render, and the resulting link set up
-		columns: [
-			{ data: "fname", "width": "100px", "title": "First Name" },
-			{ data: "lname", "width": "100px", "title": "Last Name" },
-			{ data: "att_id_use", "width": "77px", "title": "ID" },
-			{ data: "enroll_date", "width": "100px", "title": "Enrolled"},
-			{ data: "drop_date", "width": "100px", "title": "Drop Date"},
-			{ data: "id", "width": "40px", "title": "ID", "visible": false}
-		] }
-	);
-	$('#detail-roster-list-tbl td').click(function() {
-		let tblref = $('#detail-roster-list-tbl').DataTable();
-		let rowidx = tblref.cell( this ).index().row;
-		// recall that a 'row' in a jquery DataTable is an object - a rather complex object, so get a reference to it here
-		let refinfo = tblref.rows(rowidx).data();
-		// may want to console.log() the refinfo item if there is a problem with understanding what is available
-		// $("#name1o1").html(refinfo[0].fname + ' ' + refinfo[0].lname);
-		show_roster_1o1(refinfo);
-		$('#detail-roster-list-tbl tbody tr').removeClass('row-selected');
-		$(this).parent().addClass('row-selected');
-		// add row selection highlight stuff - remove existing highlight, then add back
-		var column_num = parseInt( $(this).index() ) + 1;
-		var row_num = parseInt( $(this).parent().index() ) + 1;
-	});
-}
-
-
-function show_roster_1o1(refinfo) {
-	// get the data for the person from the global object array
-	// $('#name1o1').html(
-	// 		'<p>' + refinfo[0].fname + ' ' + refinfo[0].lname + '</p>' +
-	// 		'<p>ID: ' + refinfo[0].id + '</p>'
-	// 		);
-	$('#name1o1').remove();
-	if ((refinfo[0].id % 2) == 1) {
-		$(".samp1o1").removeClass('hidediv');
-	} else {
-		$(".samp1o1").addClass('hidediv');
-	}
-
-	$('#via1').remove();
-	$('.viasamp').removeClass('hidediv');
-	if ((refinfo[0].id % 3) == 1) {
-		$('.viasamp').html('1 VIA');
-	} else {
-		$('.viasamp').html('No VIA yet');
-	}
-
 }
 
 function resize_div (div_ident, bump_t = 0, bump_b = 0) {
